34c2b3d9e60802a0d5dae7573df2b277a36bd950
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-06-10 14:52:45 +0300

    Install dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { Button } from '@heroui/button';
 import { useState } from 'react';
 
 function App() {
     const [count, setCount] = useState(0);
 
-    return <div className="bg-blue-100">Hello!</div>;
+    return (
+        <div className="center-all h-full w-full p-12">
+            <div className="col gap-4">
+                <div className="bg-blue-100 py-1 text-center font-medium">{count}</div>
+
+                <Button
+                    color="default"
+                    variant="bordered"
+                    onPress={() => {
+                        setCount(count + 1);
+                    }}
+                >
+                    Press
+                </Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+const { heroui } = require('@heroui/theme');
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx,mdx}',
+        './node_modules/@heroui/theme/dist/components/(alert|button|dropdown|form|input|modal|pagination|select|skeleton|spinner|tabs|ripple|menu|divider|popover|listbox|scroll-shadow).js',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                mona: ['Mona Sans', 'sans-serif'],
+                robotoMono: ['Roboto Mono', 'serif'],
+            },
+            flex: {
+                0: '0',
+                2: '2 1 0%',
+                3: '3 1 0%',
+                4: '4 1 0%',
+            },
+            colors: {
+                body: '#0b0b47',
+                light: '#fcfcfd',
+                primary: '#1b47f7',
+            },
+            outlineWidth: {
+                3: '3px',
+                6: '6px',
+            },
+            boxShadow: {
+                round: '0 0px 2px 0 rgb(0 0 0 / 0.05)',
+            },
+        },
+        screens: {
+            xs: '400px',
+            sm: '480px',
+            md: '768px',
+            layoutBreak: '836px',
+            lg: '1024px',
+            larger: '1232px',
+            xl: '1410px',
+            '2xl': '1536px',
+        },
+    },
+
+    darkMode: 'class',
+    plugins: [heroui()],
+};
