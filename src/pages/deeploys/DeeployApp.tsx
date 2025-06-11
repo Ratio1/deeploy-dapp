7ca6da00dcb33a467f13fdb4ce6cd03ddc1b5153
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-06-11 19:20:53 +0300

    Deeploy App initial design

--- src/pages/deeploys/DeeployApp.tsx
+++ src/pages/deeploys/DeeployApp.tsx
@@ -1,7 +1,56 @@
+import { Button } from '@heroui/button';
+import { CardWithHeader } from '@shared/cards/CardWithHeader';
+import { RiBox3Line, RiFileCodeLine } from 'react-icons/ri';
+
 function DeeployApp() {
     return (
-        <div className="center-all w-full flex-1">
-            <div>Deeploy App</div>
+        <div className="w-full flex-1">
+            <div className="grid w-full grid-cols-2 gap-5">
+                <CardWithHeader icon={<RiBox3Line />} title="Deploy an App">
+                    <div className="col h-full w-full gap-4">
+                        <div>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                            and scrambled it to make a type specimen book.
+                        </div>
+
+                        <div className="col gap-2">
+                            <div className="text-center text-lg font-medium">Choose a template</div>
+
+                            <div className="center-all gap-3">
+                                <Button color="primary" variant="flat" size="sm" onPress={() => {}}>
+                                    <div className="text-sm">Generic</div>
+                                </Button>
+
+                                <Button color="primary" variant="flat" size="sm" onPress={() => {}}>
+                                    <div className="text-sm">Native</div>
+                                </Button>
+
+                                <Button color="primary" variant="flat" size="sm" onPress={() => {}}>
+                                    <div className="text-sm">Service</div>
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </CardWithHeader>
+
+                <CardWithHeader icon={<RiFileCodeLine />} title="Deploy a Template-App" isDisabled>
+                    <div className="col h-full w-full justify-between">
+                        <div className="text-slate-500">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur.
+                        </div>
+
+                        <div className="center-all">
+                            <Button color="default" size="sm" isDisabled>
+                                <div className="text-sm">Choose Template</div>
+                            </Button>
+                        </div>
+                    </div>
+                </CardWithHeader>
+            </div>
         </div>
     );
 }
---
+++ src/shared/cards/CardWithHeader.tsx
@@ -0,0 +1,34 @@
+import { cn } from '@heroui/theme';
+import { FunctionComponent, JSX, PropsWithChildren } from 'react';
+
+interface Props {
+    icon: JSX.Element;
+    title: string;
+    label?: JSX.Element;
+    isDisabled?: boolean;
+}
+
+export const CardWithHeader: FunctionComponent<PropsWithChildren<Props>> = ({ children, icon, title, label, isDisabled }) => {
+    return (
+        <div className="col gap-0 overflow-hidden rounded-2xl border border-[#e3e4e8] bg-light">
+            <div className="bg-slate-100 px-6 py-4 larger:px-8 larger:py-6">
+                <div className="row justify-between">
+                    <div className="row gap-2 lg:gap-2.5">
+                        <div
+                            className={cn('rounded-full bg-primary p-1.5 text-[20px] text-white', isDisabled && 'bg-slate-500')}
+                        >
+                            {icon}
+                        </div>
+                        <div className={cn('text-base font-semibold leading-6 larger:text-lg', isDisabled && 'text-slate-600')}>
+                            {title}
+                        </div>
+                    </div>
+
+                    {!!label && <div>{label}</div>}
+                </div>
+            </div>
+
+            <div className="h-full px-6 py-4 lg:px-8 lg:py-6">{children}</div>
+        </div>
+    );
+};
