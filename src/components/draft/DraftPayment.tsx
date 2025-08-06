c5e768b1570dc14dd233c7429a912fd59c94c5f5
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-08-06 17:20:23 +0300

    Sign and deploy multiple jobs

--- src/components/draft/DraftPayment.tsx
+++ src/components/draft/DraftPayment.tsx
@@ -65,7 +65,6 @@ export default function DraftPayment({ project, jobs }: { project: Project; jobs
     const generateNonce = () => {
         const now = new Date();
         const unixTimestamp = now.getTime();
-        console.log('generateNonce', now, unixTimestamp);
         return `0x${unixTimestamp.toString(16)}`;
     };
 
@@ -253,6 +252,31 @@ export default function DraftPayment({ project, jobs }: { project: Project; jobs
         });
     };
 
+    const signAndBuildRequest = async (jobId: number, projectHash: string, payload: any) => {
+        const payloadWithIdentifiers = {
+            ...payload,
+            job_id: jobId,
+            project_id: projectHash,
+        };
+
+        const message = buildDeeployMessage(payloadWithIdentifiers);
+
+        const signature = await signMessageAsync({
+            account: address,
+            message,
+        });
+
+        const request = {
+            ...payloadWithIdentifiers,
+            EE_ETH_SIGN: signature,
+            EE_ETH_SENDER: address,
+        };
+
+        console.log(`(${payload.app_alias}) request`, request);
+
+        return request;
+    };
+
     const onPayAndDeploy = async () => {
         if (!jobs) {
             return;
@@ -264,10 +288,9 @@ export default function DraftPayment({ project, jobs }: { project: Project; jobs
         }
 
         setLoading(true);
+        deeployFlowModalRef.current?.open(jobs.length);
 
         const projectHash = keccak256(toBytes(project.uuid));
-
-        console.log('[DraftPayment] projectHash', projectHash);
 
         const args = jobs.map((job) => {
             const containerType: ContainerOrWorkerType = getContainerOrWorkerType(job.jobType, job.specifications);
@@ -276,8 +299,6 @@ export default function DraftPayment({ project, jobs }: { project: Project; jobs
             const diffFn = environment === 'mainnet' ? differenceInDays : differenceInHours;
 
             const durationInEpochs = diffFn(expiryDate, new Date());
-
-            console.log(`Duration in epochs: ${durationInEpochs}`);
 
             const lastExecutionEpoch = BigInt(getCurrentEpoch() + durationInEpochs);
 
@@ -288,8 +309,6 @@ export default function DraftPayment({ project, jobs }: { project: Project; jobs
                 numberOfNodesRequested: BigInt(job.specifications.targetNodesCount),
             };
         });
-
-        console.log(`Current Epoch: ${getCurrentEpoch()}`, `Last Execution Epoch: ${args[0].lastExecutionEpoch}`);
 
         const txHash = await walletClient.writeContract({
             address: escrowContractAddress,
@@ -319,44 +338,54 @@ export default function DraftPayment({ project, jobs }: { project: Project; jobs
                 .filter((log) => log !== null && log.eventName === 'JobCreated');
 
             const jobIds = jobCreatedLogs.map((log) => log.args.jobId);
-
             const payloads = getJobPayloads(jobs);
 
-            for (let index = 0; index < payloads.length; index++) {
-                const payload = payloads[index];
-                const jobId = Number(jobIds[index]);
+            deeployFlowModalRef.current?.progress('signMessages');
 
-                const payloadWithIdentifiers = {
-                    ...payload,
-                    job_id: jobId,
-                    project_id: projectHash,
-                };
+            const requests = await Promise.all(
+                payloads.map((payload, index) => {
+                    const jobId = Number(jobIds[index]);
+                    return signAndBuildRequest(jobId, projectHash, payload);
+                }),
+            );
 
-                const message = buildDeeployMessage(payloadWithIdentifiers);
+            deeployFlowModalRef.current?.progress('callDeeployApi');
 
-                const signature = await signMessageAsync({
-                    account: address,
-                    message,
-                });
+            const responses = await Promise.allSettled(
+                requests.map((request) => {
+                    return createPipeline(request);
+                }),
+            );
 
-                const request = {
-                    ...payloadWithIdentifiers,
-                    EE_ETH_SIGN: signature,
-                    EE_ETH_SENDER: address,
-                };
+            // Check for any failed deployments
+            const failedJobs = responses.filter((response) => response.status === 'rejected');
+            const successfulJobs = responses.filter((response) => response.status === 'fulfilled');
 
-                console.log('createPipeline', request);
-
-                const response = await createPipeline(request);
-
-                console.log('Response', response);
+            if (failedJobs.length > 0) {
+                console.error('Some jobs failed to deploy:', failedJobs);
+                toast.error(`${failedJobs.length} job(s) failed to deploy.`);
             }
 
-            await sleep(2000); // Wait for the allowance to be updated
+            if (successfulJobs.length > 0) {
+                console.log(
+                    'Successfully deployed jobs:',
+                    successfulJobs.map((r) => (r as PromiseFulfilledResult<any>).value),
+                );
+                toast.success(`${successfulJobs.length} job(s) deployed successfully.`);
+            }
+
+            if (successfulJobs.length === jobs.length) {
+                // deeployFlowModalRef.current?.progress('done'); TODO: Uncomment
+            }
+
+            console.log('All deployment responses:', responses);
+
             await fetchAllowance();
         } else {
             toast.error('Deployment failed, please try again.');
         }
+
+        deeployFlowModalRef.current?.close();
     };
 
     const approve = async () => {
