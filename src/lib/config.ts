2ae17412987355ff16c87dc67e3a2ccb713326bb
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-08-19 20:19:28 +0300

    Remove debugging log

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -73,11 +73,6 @@ const domainMainnet = 'deeploy.ratio1.ai';
 const domainDevnet = 'devnet-deeploy.ratio1.ai';
 const domainTestnet = 'testnet-deeploy.ratio1.ai';
 
-console.log('Mainnet', {
-    isMainnet: domain === domainMainnet,
-    isVercel: domain === domainVercel,
-});
-
 export const domains = {
     mainnet: domainMainnet,
     devnet: domainDevnet,
