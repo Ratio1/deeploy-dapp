fe95f0b4aa455001ab7b83dcc459918b732aa600
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-10-06 11:42:04 +0300

    Squashed commit of the following:
    commit 2b8be25f27732cde500d9975c50e5257457369e5
    Author: wzrdx <128477299+wzrdx@users.noreply.github.com>
    Date:   Mon Oct 6 11:38:10 2025 +0300
    
        feat: Implement burn report
    
    commit c2de0f05fc0b243123ec19fbe8da869c71ef5783
    Author: wzrdx <128477299+wzrdx@users.noreply.github.com>
    Date:   Fri Oct 3 17:30:10 2025 +0300
    
        feat: Add DateRangePicker comonent

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,20 +1,21 @@
+import BurnReport from '@components/account/BurnReport';
 import Invoicing from '@components/account/Invoicing';
 import Overview from '@components/account/Overview';
 import { routePath } from '@lib/routes/route-paths';
 import CustomTabs from '@shared/CustomTabs';
 import { useEffect, useState } from 'react';
-import { RiApps2Line, RiBillLine } from 'react-icons/ri';
+import { RiApps2Line, RiBillLine, RiFireLine } from 'react-icons/ri';
 import { useNavigate } from 'react-router-dom';
 
 function Account() {
-    const [selectedTab, setSelectedTab] = useState<'overview' | 'invoicing'>('overview');
+    const [selectedTab, setSelectedTab] = useState<'overview' | 'invoicing' | 'burn-report'>('overview');
     const navigate = useNavigate();
 
     useEffect(() => {
         const params = new URLSearchParams(window.location.search);
         const tab = params.get('tab');
 
-        if (tab && (tab === 'overview' || tab === 'invoicing')) {
+        if (tab && (tab === 'overview' || tab === 'invoicing' || tab === 'burn-report')) {
             setSelectedTab(tab);
         }
     }, [window.location.search]);
@@ -33,6 +34,15 @@ function Account() {
                         title: 'Invoicing',
                         icon: <RiBillLine />,
                     },
+                    {
+                        key: 'burn-report',
+                        title: 'Burn Report',
+                        icon: (
+                            <div className="-mr-0.5">
+                                <RiFireLine />
+                            </div>
+                        ),
+                    },
                 ]}
                 selectedKey={selectedTab}
                 onSelectionChange={(key) => {
@@ -42,6 +52,7 @@ function Account() {
 
             {selectedTab === 'overview' && <Overview />}
             {selectedTab === 'invoicing' && <Invoicing />}
+            {selectedTab === 'burn-report' && <BurnReport />}
         </div>
     );
 }
