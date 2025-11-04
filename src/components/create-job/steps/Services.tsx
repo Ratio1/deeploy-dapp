a8bad8a8ec5f54b1738a068c5f2a2eeea17705f8
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-11-04 19:56:05 +0200

    style: Selectable service cards

--- src/components/create-job/steps/Services.tsx
+++ src/components/create-job/steps/Services.tsx
@@ -1,7 +1,7 @@
 import services from '@data/services';
 import { Skeleton } from '@heroui/skeleton';
 import { loadServiceLogo } from '@lib/assets/serviceLogos';
-import { SlateCard } from '@shared/cards/SlateCard';
+import clsx from 'clsx';
 import { useEffect, useState } from 'react';
 
 function ServiceLogo({ filename, name }: { filename: string; name: string }) {
@@ -34,16 +34,32 @@ function ServiceLogo({ filename, name }: { filename: string; name: string }) {
     );
 }
 
+const SelectableCard = ({ children, isSelected, onPress }) => {
+    return (
+        <div
+            className={clsx('cursor-pointer rounded-lg border-2 bg-slate-100 p-4 transition-all', {
+                'border-transparent hover:border-slate-200': !isSelected,
+                'border-primary': isSelected,
+            })}
+            onClick={onPress}
+        >
+            {children}
+        </div>
+    );
+};
+
 export default function Services() {
+    const [selectedIndex, setSelectedIndex] = useState<number | undefined>();
+
     // Init
     useEffect(() => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     }, []);
 
     return (
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-3">
             {services.map((service, index) => (
-                <SlateCard key={index}>
+                <SelectableCard key={index} isSelected={selectedIndex === index} onPress={() => setSelectedIndex(index)}>
                     <div className="row gap-3.5">
                         <ServiceLogo filename={service.logo} name={service.name} />
 
@@ -52,7 +68,7 @@ export default function Services() {
                             <div className="compact leading-none text-slate-400">{service.description}</div>
                         </div>
                     </div>
-                </SlateCard>
+                </SelectableCard>
             ))}
         </div>
     );
--- src/data/services.ts
+++ src/data/services.ts
@@ -1,23 +1,37 @@
+export type Service = {
+    id: number;
+    name: string;
+    description: string;
+    image: string;
+    port: number;
+    inputs: { key: string; label: string }[];
+    envVars: { key: string; value: string }[];
+    logo: string;
+};
+
 export default [
     {
+        id: 1,
         name: 'PostgreSQL',
         description: 'PostgreSQL single instance',
         image: 'postgres:17',
         port: 5432,
         inputs: [{ key: 'POSTGRES_PASSWORD', label: 'PostgreSQL Password' }],
-        envVars: [], // { key, value }
+        envVars: [],
         logo: 'postgresql.svg',
     },
     {
+        id: 2,
         name: 'MySQL',
         description: 'MySQL single instance',
         image: 'mysql',
         port: 3306,
         inputs: [{ key: 'MYSQL_ROOT_PASSWORD', label: 'MySQL Root Password' }],
-        envVars: [], // { key, value }
+        envVars: [],
         logo: 'mysql.svg',
     },
     {
+        id: 3,
         name: 'MongoDB',
         description: 'MongoDB single instance',
         image: 'mongodb',
@@ -26,10 +40,11 @@ export default [
             { key: 'MONGO_INITDB_ROOT_USERNAME', label: 'MongoDB Root Username' },
             { key: 'MONGO_INITDB_ROOT_PASSWORD', label: 'MongoDB Root Password' },
         ],
-        envVars: [], // { key, value }
+        envVars: [],
         logo: 'mongodb.svg',
     },
     {
+        id: 4,
         name: 'Strapi',
         description: 'Next-gen headless CMS',
         image: 'strapi',
@@ -38,7 +53,7 @@ export default [
             { key: 'STRAPI_ADMIN_USERNAME', label: 'Strapi Admin Username' },
             { key: 'STRAPI_ADMIN_PASSWORD', label: 'Strapi Admin Password' },
         ],
-        envVars: [], // { key, value }
+        envVars: [],
         logo: 'strapi.svg',
     },
 ];
