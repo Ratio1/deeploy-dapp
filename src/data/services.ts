485accdddecb1bbc363d8742ce941e9f4b5b90a6
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-11-06 16:02:28 +0200

    add_service: n8n (#30)
    * add_service: n8n
    
    * fix: Add logo and update service

--- src/data/services.ts
+++ src/data/services.ts
@@ -83,6 +83,16 @@ export default [
         logo: 'strapi.svg',
         color: 'violet',
     },
+    {
+        id: 5,
+        name: 'n8n',
+        description: 'Workflow automation platform',
+        image: 'n8nio/n8n',
+        port: 5678,
+        inputs: [],
+        logo: 'n8n.svg',
+        color: 'pink',
+    },
 ] as Service[];
 
 export type { Service };
